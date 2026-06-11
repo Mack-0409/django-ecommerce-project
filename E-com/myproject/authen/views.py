3bcf2c6da1dc3eadda4926697a5f2321fdb314a8
Mack-0409 <mayanksingh0409@gmail.com>
2026-06-11 23:14:44 +0530

    authen login/register/logout

--- E-com/myproject/authen/views.py
+++ E-com/myproject/authen/views.py
@@ -1,14 +1,46 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def login_(request):
-    return render(request, 'login_.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login_.html', {'error':'Username or Password is wrong....!'})
+    return render(request, 'login_.html')   
 
 def logout_(request):
-    return render(request, 'logout_.html')
+    logout(request)
+    return redirect('login_')
 
 def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['pasw']
+        confirm_password = request.POST['confirm_pasw']
+        try:
+            user = User.objects.get(username = username)
+            return render(request, 'register.html', {'error':'UserName is already taken...!'})
+        except:
+            User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                password = password,
+            )
+            return redirect('login_')
     return render(request, 'register.html')
 
 '''
