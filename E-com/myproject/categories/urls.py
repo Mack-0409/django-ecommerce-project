bf5c6af2152c5d30edac0bf5d906ae0f3885e9c2
Mack-0409 <mayanksingh0409@gmail.com>
2026-07-06 14:51:21 +0530

    categories

--- E-com/myproject/categories/urls.py
+++ E-com/myproject/categories/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('nike/', nike, name='nike'),
     path('bacca/', bacca, name='bacca'),
+    path('adidas/', adidas, name='adidas'),
+    path('puma/', puma, name='puma'),
+    path('reebok/', reebok, name='reebok'),
+    path('polo/', polo, name='polo'),
+    path('new-balance/', new_balance, name='new_balance'),
 ]
--- E-com/myproject/categories/views.py
+++ E-com/myproject/categories/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def nike(request):
     return render(request, 'nike.html')
 
 def bacca(request):
     return render(request, 'bacca.html')
+
+def adidas(request):
+    return render(request, 'adidas.html')
+
+def puma(request):
+    return render(request, 'puma.html')
+
+def reebok(request):
+    return render(request, 'reebok.html')
+
+def polo(request):
+    return render(request, 'polo.html')
+
+def new_balance(request):
+    return render(request, 'new_balance.html')
