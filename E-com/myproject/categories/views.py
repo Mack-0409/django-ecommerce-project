34b02f43e9714470b60a325fa2228da9be0e8541
Mack-0409 <mayanksingh0409@gmail.com>
2026-07-12 23:36:53 +0530

    categories

--- E-com/myproject/categories/views.py
+++ E-com/myproject/categories/views.py
@@ -20,3 +20,45 @@ def polo(request):
 
 def new_balance(request):
     return render(request, 'new_balance.html')
+
+def calvin_klein(request):
+    return render(request, 'calvin_klein.html')
+
+def converse(request):
+    return render(request, 'converse.html')
+
+def fila(request):
+    return render(request, 'fila.html')
+
+def gucci(request):
+    return render(request, 'gucci.html')    
+
+def H_and_M(request):
+    return render(request, 'H_and_M.html')
+
+def levis(request): 
+    return render(request, 'levis.html')    
+
+def off_white(request):
+    return render(request, 'off_white.html')        
+
+def skechers(request):
+    return render(request, 'skechers.html')
+
+def supreme(request):
+    return render(request, 'supreme.html')
+
+def timberland(request):
+    return render(request, 'timberland.html')
+
+def tommy_hilfiger(request):
+    return render(request, 'tommy_hilfiger.html')
+
+def under_armour(request):
+    return render(request, 'under_armour.html')
+
+def vans(request):
+    return render(request, 'vans.html')     
+
+def zara(request):
+    return render(request, 'zara.html') 
